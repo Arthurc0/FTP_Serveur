ffe920394808f834be576ac9f76cb4add3f8a67b
Arthurc0 <chaff.arthur@gmail.com>
2022-02-25 17:42:35 +0100

    Création du projet FTP_Serveur

---
+++ FTP_Serveur/src/vue/CommandeGET.java
@@ -0,0 +1,15 @@
+package vue;
+
+import java.io.PrintStream;
+
+public class CommandeGET extends Commande {
+	
+	public CommandeGET(PrintStream ps, String commandeStr) {
+		super(ps, commandeStr);
+	}
+
+	public void execute() {
+		ps.println("La commande get n'est pas encore implémentée");
+	}
+
+}
